73d066479f1551c551840c49afa4bdbf31705c2e
Lisha Liang <lishaliang1999@gmail.com>
2022-09-08 00:02:48 +0000

    calculate letter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#tweet-text").on('keydown', function () {
+    const remain = 140 - this.value.length    
+    let characterCounter = document.getElementById('counter')
+    characterCounter.textContent = remain
+
+    if (remain > 0) {
+      characterCounter.style.color = "black";
+    } else {
+      characterCounter.style.color = "red";
+    }
+
+
+  })
+});
+
+
+
